792e8160479410b32d3834453bd4006f3d26239b
shriparna <s_v_kamat@yahoo.com>
2023-03-21 17:25:47 -0700

    Completed PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,25 @@
 # main.py
 # This program will cover the challenge for PyBank
 # Logic: 
-# 1. Read CSV file from resources (Resources/budget_data.csv)
-# 2. Create an empty list for date and profit_loss
-# 3. Create a variable for records, prev_difference, greatest_increase, greatest_decrease and intitialize them with 0
-# 4. Traverse through the file and skip the header row
-# 5. Increement the counter of the records and increment sum of profilt_loss
-# 6. Check if current_diff is greater than the previous one then store new as the greatest and record the period
-# 7. Check if current_diff is lower than the previous one then store new as the lowest and record the period
-# 8. After the end of file print the values
+# 1. Read CSV file from Resources (Resources/budget_data.csv) 
+# 2. Create variables totalmonths, total, difference
+# 3. Create variables for records, prev_profitloss, current_profitloss, greatest_increase, greatest_decrease and intitialize them with 0
+# 4. Create an empty list for output so that it can be printed on the console and written to the output file at the same time
+# 5. Traverse through the file and skip the header row
+# 6. For the first row note down the profit_loss as previous profit_loss
+# 7. Increment the counter of the records in totalmonths and increment sum of profilt_loss in total
+# 8. Calculate difference by subtracting previous profit_loss from current profil_loss and this difference to cumulative difference
+# 9. Check if current_diff is greater than the previous one then store new as the greatest and record the period
+#10. Check if current_diff is lower than the previous one then store new as the lowest and record the period
+#11. Assign current profit_loss to previous profit_loss
+#12. After the end of input file 
+#13. Open the output file as CSV in anaysis/financial_analysis.csv
+#14. From the output list print the output values on the console and print the details in the output file
 
 import csv
 
 csvfilepath = "Resources/budget_data.csv"
-outputpath = "analysis/output.txt"
+outputfilepath = "analysis/financial_analysis.csv"
 totalmonths = 0
 total = 0
 prev_pl = 0
@@ -23,11 +29,16 @@ cum_diff = 0
 avg_change = 0
 greatest_increase = 0
 greatest_decrease = 0
+outputtext = []
 
 with open (csvfilepath, 'r') as pybankfile:
     csvreader = csv.reader (pybankfile, delimiter=',')
 
-    csvheader = next(csvreader) # skip the header
+    csvheader = next(csvreader) # skip the header row
+
+    # append the header row to the outputtext
+    outputtext.append("Financial Analysis")
+    outputtext.append("------------------------------")
 
     for row in csvreader:
         totalmonths = totalmonths + 1 
@@ -45,12 +56,21 @@ with open (csvfilepath, 'r') as pybankfile:
         cum_diff = cum_diff + diff
         prev_pl = curr_pl
       
-avg_change = round(cum_diff / (totalmonths - 1), 2) # since it start from row 2, we subtract the count by 1     
-print("Financial Analysis")
-print(" ")
-print("------------------------------")
-print(f"Total Months: {totalmonths}")
-print(f"Total : {total}")
-print(f"Average Change: {avg_change}")
-print(f"Greatest Increase in Profits: {gi_period} (${greatest_increase})")
-print(f"Greatest Decrease in Profits: {gd_period} (${greatest_decrease})")
+avg_change = round(cum_diff / (totalmonths - 1), 2) # since it start from row 2, we subtract the count by 1  
+# append the calculations to the ouptuttext   
+outputtext.append(f"Total Months: {totalmonths}")
+outputtext.append(f"Total : {total}")
+outputtext.append(f"Average Change: {avg_change}")
+outputtext.append(f"Greatest Increase in Profits: {gi_period} (${greatest_increase})")
+outputtext.append(f"Greatest Decrease in Profits: {gd_period} (${greatest_decrease})")
+
+# print values to a CSV file and the console
+with open (outputfilepath, 'w') as csvoutputfile:
+    #Initialize the csv.writer
+    csvoutput = csv.writer(csvoutputfile)
+
+    #There is no specific header as the output is more like a text format
+    for line in outputtext:
+        print(line)
+        csvoutput.writerow([line])
+        
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# python-challenge
-Module 3: Python Challenge
+# Module 3: python-challenge
+
+## Author: Shridhar Kamat.
+<hr>
+
+### Details of the program or pseudo logic
+#### Shridhar Kamat
+#### Date: 3/16/2023
+<hr>
+Package Contents:
+
+1. PyBank
+
+    a. [main.py]()
+
+    b. [budget_data.csv]()  (Input File)
+
+    c. [financial_analysis.csv]()   (Output File)
+    
+2. PyPoll
+
+    a. [main.py]()
+
+    b, [election_data.csv]()    (Input File)
+
+    c. [election_results.csv]()  (Output File)
+
+3. [README.md](https://github.com/shriparna/python-challenge/blob/main/README.md) 
+
+<hr>
+
+## Pseudo Logic  
+<hr>
+
+## 1. PyBank
+
+Logic: 
+-  Read CSV file from Resources (Resources/budget_data.csv) 
+- Create variables totalmonths, total, difference
+- Create variables for records, prev_profitloss, current_profitloss, greatest_increase, greatest_decrease and intitialize them with 0
+- Create an empty list for output so that it can be printed on the console and written to the output file at the same time
+- Traverse through the file and skip the header row
+- For the first row note down the profit_loss as previous profit_loss
+- Increment the counter of the records in totalmonths and increment sum of profilt_loss in total
+- Calculate difference by subtracting previous profit_loss from current profil_loss and this difference to cumulative difference
+- Check if current_diff is greater than the previous one then store new as the greatest and record the period
+- Check if current_diff is lower than the previous one then store new as the lowest and record the period
+- Assign current profit_loss to previous profit_loss
+- After the end of input file 
+- Open the output file as CSV in anaysis/financial_analysis.csv
+- From the output list print the output values on the console and print the details in the output file
+<hr>
+
+## 2. PyPoll
+
+Logic: 
+-  Read CSV file from Resources (Resources/election_data.csv).
+- Create variables totalmonths, total, difference
+- Create a variable for records, prev_profitloss, current_profitloss, greatest_increase, greatest_decrease and intitialize them with 0
+- Create an empty list for output so that it can be printed on the console and written to the output file at the same time
+- Traverse through the file and skip the header row
+- For the first row note down the profit_loss as previous profit_loss
+- Increement the counter of the records in totalmonths and increment sum of profilt_loss in total
+- Check if current_diff is greater than the previous one then store new as the greatest and record the period
+- Check if current_diff is lower than the previous one then store new as the lowest and record the period
+- After the end of file 
+- Also open the output file as CSV in anaysis/election_results.csv
+- Print the values on the console
+- Print the details in the output file
